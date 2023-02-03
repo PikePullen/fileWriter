2a8984867bed5cc2271cb5d917b542d6807e22d4
ppullen <pppullen@gmail.com>
2023-02-03 16:23:36 -0500

    Added comments and variations of file access for absolute and relative paths

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+"""
+absolute file path begins at root, or c:/ in windows
+relative file path begins as the current working directory, such as the path of the .py file
+
+./ references the current working directory
+../ references the parent of the current working directory
+
+can use multiple "../" to move up multiple levels IE "../../"
+"""
+
 # file = open("my_file.txt")
 # contents = file.read()
 # print(contents)
 # file.close()
 
-# using the "with-as" mechanism, we remove the need to open and close a file in the other way
-# mode is read/write/append "r" or "w" or "a"
+"""
+using the "with-as" mechanism, we remove the need to open and close a file in the other way
+mode is read/write/append "r" or "w" or "a
+""""
 
-# write
-with open("my_file.txt", mode="r") as file:
+# write-1
+# with open("my_file.txt", mode="r") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+""" this works the same way as the "write" version but with a different way of getting to file"""
+# write-2
+with open("./my_file.txt", mode="r") as file:
     contents = file.read()
     print(contents)
 
